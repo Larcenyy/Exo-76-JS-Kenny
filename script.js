75f9dea3afe9805809059a1bffa55b0a15d3b761
Rémy <remyc59440@hotmail.com>
2022-11-22 15:55:52 +0100

    exo keny ok

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
+let zone = document.getElementById("zone");
+zone.style.backgroundColor = "red";
+zone.style.height = "500px";
+zone.style.width = "50px";
+
+let count = 0;
+
+let counter = document.getElementById("counter");
+counter.style.color = "red";
+counter.innerText = "Kenny est mort, " + count + " fois";
+
+
+function reset() {
+    let t = document.getElementById('kenny');
+    console.log(t);
+    t.style.top = "200px"
+    t.style.left = "200px"
+    count++;
+    counter.innerText = "Kenny est mort, " + count + " fois";
+}
+
+
 
 
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
-    t = t - 10;
-    document.getElementById('kenny').style.top = t + 'px';
+    console.log(t);
+    if(t > 0){
+        t = t - 10; // Déplacement
+        document.getElementById('kenny').style.top = t + 'px';
+    }
 });
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    console.log(t);
+    if(t < 460){
+        t = t + 10; // Déplacement
+        document.getElementById('kenny').style.top = t + 'px';
+    }
+});
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    console.log(t);
+    if(t < 460){
+        t = t + 10; // Déplacement
+        document.getElementById('kenny').style.left = t + 'px';
+    }
+});
+document.getElementById('left').addEventListener('click',function() {
+
+    let t = parseInt(document.getElementById('kenny').style.left);
+    console.log(t);
+    if(t > 0){
+        t = t - 10; // Déplacement
+        document.getElementById('kenny').style.left = t + 'px';
+        if(t < 40){
+            reset()
+        }
+    }
+});
+
